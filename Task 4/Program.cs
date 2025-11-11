a2934e57deb109550ff72165e648ee4718ce1154
sandhya-sunset <sandhyamajhi77@gmail.com>
2025-11-11 11:54:04 +0545

    Task 5

--- Task 4/Program.cs
+++ Task 4/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace Task_4
+﻿using System;
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        Console.Write("Enter a number (1–7): ");
+        int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+        switch (dayNumber)
         {
-            Console.WriteLine("Hello, World!");
+            case 1:
+                Console.WriteLine("Sunday");
+                break;
+            case 2:
+                Console.WriteLine("Monday");
+                break;
+            case 3:
+                Console.WriteLine("Tuesday");
+                break;
+            case 4:
+                Console.WriteLine("Wednesday");
+                break;
+            case 5:
+                Console.WriteLine("Thursday");
+                break;
+            case 6:
+                Console.WriteLine("Friday");
+                break;
+            case 7:
+                Console.WriteLine("Saturday");
+                break;
+            default:
+                Console.WriteLine("Invalid day number");
+                break;
         }
     }
 }
